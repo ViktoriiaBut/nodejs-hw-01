c5163f73f8d979d4f3e91a100047ee727eb71b7c
ViktoriiaBut <zironka.tory@gmail.com>
2025-05-30 23:57:13 +0100

    S123

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,33 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from "../utils/createFakeContact";
+import { readContacts } from "../utils/readContacts";
+import { writeContacts } from "../utils/writeContacts";
+
+const generateContacts = async (number) => {
+ let createContacts = [];
+ for (let i = 0; i < number; i++) {
+    createContacts.push(createFakeContact());
+ }
+
+ let existingContacts = [];
+    try {
+        const data = await readContacts();
+        existingContacts = data;
+    } catch (error) {
+        if (error !== "ENOENT") {
+            console.log(error);
+            return;
+    }
+};
+const updateContacts = [...existingContacts, ...createContacts];
+
+try {
+  await writeContacts(updateContacts);
+} catch (error) {
+    console.log(error);
+
+}
+};
 
 generateContacts(5);
+
+
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,12 @@
-import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+
+export const readContacts = async () => {
+    try {
+     const data = await fs.readFile(PATH_DB, 'utf8');
+     return JSON.parse(data);
+    } catch (error) {
+    console.log('Error:', error.massege);
+    }
+};
